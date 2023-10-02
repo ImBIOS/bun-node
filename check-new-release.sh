62ea1d167b306b8cb895a914537bf64cf7df5940
Imamuzzaki Abu Salam <imamuzzaki@gmail.com>
2023-10-02 11:55:53 +0700

    chore(build-docker): update build and push actions

--- check-new-release.sh
+++ check-new-release.sh
@@ -8,8 +8,8 @@ curl -s https://api.github.com/repos/$REPO/releases/latest | jq -r '.tag_name' >
 if [[ -f last_$FILE.txt && $(cat last_$FILE.txt) != $(cat latest_$FILE.txt) ]]; then
   echo "New version found for $FILE."
   cat latest_$FILE.txt >last_$FILE.txt
-  exit 0
+  echo "is-out-of-date=true" >>"$GITHUB_OUTPUT"
 else
   echo "No new version found for $FILE."
-  exit 1
+  echo "is-out-of-date=false" >>"$GITHUB_OUTPUT"
 fi
