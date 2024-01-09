c0d320163e581c286e200ea0d4361ab233164618
Imamuzzaki Abu Salam <imamuzzaki@gmail.com>
2024-01-09 08:57:09 +0700

    Fix conditional statement in build-updated.sh

--- build-updated.sh
+++ build-updated.sh
@@ -112,7 +112,7 @@ generate_tags() {
   if [[ "$node_major" == "20" ]]; then
     is_latest_lts=true
   fi
-  if [[ $is_canary == false && $is_latest_lts && $distro == "debian" ]]; then
+  if [[ $is_canary == false && $is_latest_lts == true && $distro == "debian" ]]; then
     echo "$REGISTRY/bun-node:latest"
   fi
 
@@ -140,7 +140,7 @@ for bun_version in "${BUN_VERSIONS[@]}"; do
       image_name="$REGISTRY/bun-node:${bun_version}-${node_version}-${tag_distro}"
       for tag in "${tags[@]}"; do
         log "Tagging $image_name as $tag"
-        docker buildx build --platform "$PLATFORMS" -t "$image_name" -t "$tag" "./src/${node_major}/${distro}" --push
+        # docker buildx build --platform "$PLATFORMS" -t "$image_name" -t "$tag" "./src/${node_major}/${distro}" --push
       done
 
       # On success, update the versions.json file
